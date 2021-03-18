7f47584426cfadde7fc03b309fc95dcbfb7499da
Mykel <songsongha@gmail.com>
2021-03-18 16:53:42 -0600

    added join game button and play functionality

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ httpServer.listen(9090, () => console.log('Listening on port 9090'))
 
 //an object to store all of the clients and their guid
 const clients = {};
+// an object to store all of the games that have been created
 const games = {};
 const wsServer = new websocketServer({
 		"httpServer": httpServer
@@ -25,9 +26,11 @@ wsServer.on("request", request => {
 		if (result.method === 'create'){
 			const clientId = result.clientId
 			const gameId = guid();
+			//storeing game data by Id
 			games[gameId] = {
 				"id": gameId,
-				"balls": 20
+				"balls": 20,
+				"clients": []
 			}
 
 			const payLoad = {
@@ -39,6 +42,61 @@ wsServer.on("request", request => {
 			con.send(JSON.stringify(payLoad));
 
 		}
+		// user wants to join a game
+		if (result.method === 'join'){
+			const clientId = result.clientId;
+			const gameId = result.gameId;
+			const game = games[gameId];
+			console.log("joined game")
+
+			if (game.clients.length >=3)
+			{
+				//sorry max players reached, need to send user some sort of message
+				return;
+			}
+
+			// assign a color to player
+			const color = {"0":"Red", "1":"Green", "2":"Blue"}[game.clients.length];
+			game.clients.push({
+				"clientId": clientId,
+				"color": color
+			})
+			//start the game if 3 people have joined
+			if(game.clients.length === 3) updateGameState();
+
+			const payLoad = {
+				"method": 'join',
+				"game": game
+			}
+
+			//loop through all clients and tell them that people have joined
+			game.clients.forEach(c=>{
+				clients[c.clientId].connection.send(JSON.stringify(payLoad));
+			})
+
+		}
+
+		//user wants to play
+		if (result.method === 'play'){
+			const clientId = result.clientId;
+			const gameId = result.gameId;
+			const ballId = result.ballId;
+
+			let state = games[gameId].state;
+			if (!state){
+				state={};
+			}
+			state[ballId] = color;
+			games[gameId].state = state;
+			const game = games[gameId]
+
+			const payLoad = {
+				"method": 'play',
+				"game": game
+
+			}
+		}
+
 	})
 
 	//generate a new clientId
@@ -56,6 +114,22 @@ wsServer.on("request", request => {
 	connection.send(JSON.stringify(payLoad))
 
 })
+
+function updateGameState(){
+	
+	for (const g of Object.keys(games)){
+		const game = games[g];
+		const payLoad = {
+				"method": 'update',
+				"game": game
+		}
+
+		game.clients.forEach(c=>{
+			client[c.clientId].connection.send(JSON.stringify(payLoad))
+		})
+	}
+	setTimeout(updateGameState, 500);
+}
 
 const guid=()=> {
   const s4=()=> Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);     
